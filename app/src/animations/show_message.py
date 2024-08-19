bf970f8bd94ccd808f3678e588e03a2a1201599d
Mo Mobarak <mo@mobarak.com>
2024-08-19 17:49:44 +0100

    wip

--- app/src/animations/show_message.py
+++ app/src/animations/show_message.py
@@ -3,12 +3,26 @@ import random
 import time
 
 from PIL import Image, ImageDraw, ImageFont
+from uniseg.graphemecluster import grapheme_clusters
 
 from animations.base_animation import BaseAnimation
 from display import Display
 from util import image_util
 
 logger = logging.getLogger(__name__)
+
+EMOJI_GLYPHS_PATH = 'assets/emoji_glyphs/32x32'
+FONT_PATH = 'assets/fonts/MILL/Canada Type - Screener SC.ttf'
+
+
+def grapheme_to_hex(grapheme: str) -> str:
+    hex_codes = [hex(ord(c)).replace('0x', '') for c in grapheme]
+    return '-'.join(hex_codes)
+
+def image_file_for_grapheme(grapheme: str) -> str:
+    hex_code = grapheme_to_hex(grapheme)
+    return f'{EMOJI_GLYPHS_PATH}/{hex_code}.png'
+
 
 class ShowMessage(BaseAnimation):
     def __init__(self, display: Display) -> None:
@@ -52,10 +66,24 @@ class ShowMessage(BaseAnimation):
 
         panel_images = [self.panel_image(c, self.word_font, (r,g,b)) for c in message]
 
+        # MODEBUG
+        panel_images.append(self.lookup_char_image('🦠'))
+
         display_images  = []
         display_images.append(image_util.display_image_from_panel_images(panel_images))
 
         return display_images
+    
+    def lookup_char_image(self, grapheme: str) -> Image:
+        image_file = image_file_for_grapheme(grapheme)
+
+        try: 
+            image = Image.open(image_file)
+            image = image.convert('RGB')
+        except FileNotFoundError as e:
+            image =  None if grapheme == ' ' else create_char_image(grapheme)
+    
+        return image
 
     async def run(self, message, **kwargs):
         self.display.clear()
@@ -73,3 +101,50 @@ class ShowMessage(BaseAnimation):
             time.sleep(flash_delay)
             self.display.setImage(final_display_image, x_offset=0, y_offset=0)
             time.sleep(flash_delay)
+
+###########################################################################
+# TODO - switch over to the panel image strategy below
+###########################################################################
+
+
+def iterate_graphemes(msg):
+    for grapheme in list(grapheme_clusters(msg)):
+        yield grapheme
+
+def images_for_message(msg, emoji_only=False):
+    return [lookup_char_image(g) for g in iterate_graphemes(msg)]
+
+def create_char_image(char: str, image_size=(64,64), font_path: str = FONT_PATH) -> Image.Image:
+    # Image dimensions and border thickness
+    img_size = image_size
+    border_thickness = 2
+
+    # Create a blank image with a black background
+    image = Image.new("RGB", img_size, "black")
+    draw = ImageDraw.Draw(image)
+
+    # Load the font
+    font_size = 60  # Adjust the font size
+    font = ImageFont.truetype(font_path, font_size) if font_path else ImageFont.load_default(font_size=font_size)
+
+    # Calculate the position to center the character
+    text_width = draw.textlength(char, font=font)
+
+    text_bbox = draw.textbbox((0, 0), char, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
+
+    text_x = (img_size[0] - text_width) // 2
+    text_y = (img_size[1] - text_height) // 2
+
+    # Draw the character in white
+    draw.text((text_x, text_y), char, font=font, fill="white")
+
+    # Draw the 2-pixel black border (already the background, so this is optional)
+    draw.rectangle(
+        [border_thickness-1, border_thickness-1, img_size[0]-border_thickness, img_size[1]-border_thickness],
+        outline="black",
+        width=border_thickness
+    )
+
+    return image
--- app/src/util/image_util.py
+++ app/src/util/image_util.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 from display import Display
 
@@ -33,74 +33,3 @@ def display_image_from_panel_images(panel_images):
         current_x += panel_width
 
     return display_image
-
-###########################################################################
-# TODO - switch over to the panel image strategy below
-###########################################################################
-
-from PIL import Image, ImageDraw, ImageFont
-from uniseg.graphemecluster import grapheme_clusters
-
-EMOJI_GLYPHS_PATH = 'assets/emoji/64x64'
-FONT_PATH = 'assets/fonts/MILL/Canada Type - Screener SC.ttf'
-
-def grapheme_to_hex(grapheme: str) -> str:
-    hex_codes = [hex(ord(c)).replace('0x', '') for c in grapheme]
-    return '-'.join(hex_codes)
-
-def image_file_for_grapheme(grapheme: str) -> str:
-    hex_code = grapheme_to_hex(grapheme)
-    return f'{EMOJI_GLYPHS_PATH}/{hex_code}.png'
-
-def lookup_char_image(grapheme: str):
-    image_file = image_file_for_grapheme(grapheme)
-
-    try: 
-        image = Image.open(image_file)
-        image = image.convert('RGB')
-    except FileNotFoundError as e:
-        return None if grapheme == ' ' else create_char_image(grapheme)
-    
-    return image
-
-def iterate_graphemes(msg):
-    for grapheme in list(grapheme_clusters(msg)):
-        yield grapheme
-
-def images_for_message(msg, emoji_only=False):
-    return [lookup_char_image(g) for g in iterate_graphemes(msg)]
-
-def create_char_image(char: str, image_size=(64,64), font_path: str = FONT_PATH) -> Image.Image:
-    # Image dimensions and border thickness
-    img_size = image_size
-    border_thickness = 2
-
-    # Create a blank image with a black background
-    image = Image.new("RGB", img_size, "black")
-    draw = ImageDraw.Draw(image)
-
-    # Load the font
-    font_size = 60  # Adjust the font size
-    font = ImageFont.truetype(font_path, font_size) if font_path else ImageFont.load_default(font_size=font_size)
-
-    # Calculate the position to center the character
-    text_width = draw.textlength(char, font=font)
-
-    text_bbox = draw.textbbox((0, 0), char, font=font)
-    text_width = text_bbox[2] - text_bbox[0]
-    text_height = text_bbox[3] - text_bbox[1]
-
-    text_x = (img_size[0] - text_width) // 2
-    text_y = (img_size[1] - text_height) // 2
-
-    # Draw the character in white
-    draw.text((text_x, text_y), char, font=font, fill="white")
-
-    # Draw the 2-pixel black border (already the background, so this is optional)
-    draw.rectangle(
-        [border_thickness-1, border_thickness-1, img_size[0]-border_thickness, img_size[1]-border_thickness],
-        outline="black",
-        width=border_thickness
-    )
-
-    return image
