48e256f1028b4c78e9ceafb38df9bcf318032c87
Mo Mobarak <mo@mobarak.com>
2024-08-29 11:43:09 +0100

    try the python emoji library

--- app/src/animations/base_animation.py
+++ app/src/animations/base_animation.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 # Configuration Constants
 ##############################################################
 _WORD_FONT_PATH = 'assets/fonts/MILL/Canada Type - Screener SC.ttf'
-_EMOJI_FONT_PATH = 'assets/fonts/Noto_Emoji/static/NotoEmoji-Medium.ttf'
-#_EMOJI_FONT_PATH = 'assets/fonts/TwitterColorEmoji-SVGinOT-14.0.2/TwitterColorEmoji-SVGinOT.ttf'
+# _EMOJI_FONT_PATH = 'assets/fonts/Noto_Emoji/static/NotoEmoji-Medium.ttf'
+_EMOJI_FONT_PATH = 'assets/fonts/TwitterColorEmoji-SVGinOT-14.0.2/TwitterColorEmoji-SVGinOT.ttf'
 
 class BaseAnimation:
     def __init__(self, display, word_font_path=_WORD_FONT_PATH, emoji_font_path=_EMOJI_FONT_PATH) -> None:
--- app/src/util/emoji.py
+++ app/src/util/emoji.py
@@ -1,9 +1,11 @@
 import os
 import random
 
+import emoji
+
 _EMOJI_GLYPH_DIR = 'assets/emoji_glyphs/32x32'
 
-def _load_emoji_list(emoji_glyph_dir: str = _EMOJI_GLYPH_DIR):
+def _emoji_list_from_glyphs(emoji_glyph_dir: str = _EMOJI_GLYPH_DIR):
     emoji_list = []
     for filename in os.listdir(emoji_glyph_dir):
         if filename.endswith('.png'):
@@ -12,14 +14,15 @@ def _load_emoji_list(emoji_glyph_dir: str = _EMOJI_GLYPH_DIR):
             emoji_list.append(emoji)
     return emoji_list
 
-emoji_list = _load_emoji_list()
+# _emoji_list = _emoji_list_from_glyphs()
+_emoji_list = list(emoji.EMOJI_DATA.keys())
 
 def is_emoji(grapheme: str) -> bool:
-    return grapheme in emoji_list
+    return grapheme in _emoji_list
 
 def make_random_quartets(num_quartets: int):
     random_quartets = []
     for _ in range(num_quartets):
-        random_quartets.append(''.join(random.choices(emoji_list, k=4)))
+        random_quartets.append(''.join(random.choices(_emoji_list, k=4)))
 
     return random_quartets
