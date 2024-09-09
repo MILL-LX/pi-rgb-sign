752dfa034cec32fd742071a7778dd6580215c655
Mo Mobarak <mo@mobarak.com>
2024-09-09 17:26:37 +0100

    Python print client - no local CUPS server required

--- app/src/util/print_util.py
+++ app/src/util/print_util.py
@@ -1,10 +1,13 @@
 import cups
 
+# lp -d ThermalPrinterAPE assets/emoji_glyphs/32x32/1f3c4-1f3fc-200d-2640-fe0f.png
+
 _printer_name = "ThermalPrinterAPE"
 _cups_server = "10.10.10.186"
-_printer_connection = None
+_printer_connection = cups.Connection(host=_cups_server)
 
-def print_to_remote_cups(file_path):
+def print_file(file_path):
+    global _printer_connection
     if not _printer_connection:
         _printer_connection = cups.Connection(host=_cups_server)    
 
@@ -13,6 +16,6 @@ def print_to_remote_cups(file_path):
 # Example usage
 if __name__ == "__main__":
     file_path = "assets/emoji_glyphs/32x32/1f3c4-1f3fc-200d-2640-fe0f.png"
-      
-    job_id = print_to_remote_cups(file_path)
+
+    job_id = print_file(file_path)
     print(f"Print job submitted with ID: {job_id}")
