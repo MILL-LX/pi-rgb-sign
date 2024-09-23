ba9a6a8d180df9dc9fd21925fc5789fb27c62e6e
Mo Mobarak <mo@mobarak.com>
2024-09-23 20:25:27 +0100

    more robust printer handling

--- app/src/util/print_util.py
+++ app/src/util/print_util.py
@@ -2,24 +2,33 @@ import cups
 
 # lp -d ThermalPrinterAPE assets/emoji_glyphs/32x32/1f3c4-1f3fc-200d-2640-fe0f.png
 
-_printer_name = "ThermalPrinterAPE"
-_cups_server = "10.10.10.186"
+_printer_name = 'ThermalPrinterAPE'
+_cups_server = '10.10.10.186' #TODO: update for printer on ApeFest Router
 _printer_connection = None
 
 def print_file(file_path):
     global _printer_connection
+    print_job_id = None
+
     if not _printer_connection:
         try:
             _printer_connection = cups.Connection(host=_cups_server)    
         except Exception as e:
-            print(f"Error connecting to printer: {e}")
-            return None
+            print(f'Error connecting to printer: {e}')
+            _printer_connection = None
 
-    return _printer_connection.printFile(_printer_name, file_path, "Print Job", {})
+    if _printer_connection:
+        try:
+            print_job_id = _printer_connection.printFile(_printer_name, file_path, 'Print Job', {})
+        except Exception as e:
+            print(f'Error printing file: {e}')
+            _printer_connection = None
+
+    return print_job_id
 
 # Example usage
-if __name__ == "__main__":
-    file_path = "assets/emoji_glyphs/32x32/1f3c4-1f3fc-200d-2640-fe0f.png"
+if __name__ == '__main__':
+    file_path = 'assets/images/apefest/printer/loser_ticket.png'
 
     job_id = print_file(file_path)
-    print(f"Print job submitted with ID: {job_id}")
+    print(f'Print job submitted with ID: {job_id}')
