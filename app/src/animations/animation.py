4a04e72542e0c73c2d3ebe458480bdb9e8868945
Mo Mobarak <mo@mobarak.com>
2024-09-27 11:21:49 +0100

    remove cruft, add types

--- app/src/animations/animation.py
+++ app/src/animations/animation.py
@@ -1,5 +1,4 @@
 import logging
-from enum import Enum
 
 from display import Display
 from display_image_generator import DisplayImageGenerator
@@ -14,7 +13,7 @@ _WORD_FONT_PATH = 'assets/fonts/MILL/Canada Type - Screener SC.ttf'
 _EMOJI_FONT_PATH = 'assets/fonts/TwitterColorEmoji-SVGinOT-14.0.2/TwitterColorEmoji-SVGinOT.ttf'
 
 class Animation:
-    def __init__(self, display, word_font_path=_WORD_FONT_PATH, emoji_font_path=_EMOJI_FONT_PATH) -> None:
+    def __init__(self, display: Display, word_font_path: str = _WORD_FONT_PATH, emoji_font_path: str = _EMOJI_FONT_PATH) -> None:
         self.display = display
         self.animation_name = self.__class__.__name__
         logger.info(f'Making a {self.animation_name} animation for a display with {display.num_panels} panels.')
