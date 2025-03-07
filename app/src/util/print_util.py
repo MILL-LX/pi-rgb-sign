cc4174495620bb70caeec7fe0fd9781e282008ef
Mo Mobarak <mo@mobarak.com>
2025-03-07 14:47:46 +0000

    print on a background thread to avoid hanging if printer is unavailable

--- app/src/util/print_util.py
+++ app/src/util/print_util.py
@@ -1,4 +1,5 @@
 import cups
+import threading
 
 _printer_name = 'ThermalPrinterAPE'
 _cups_server = 'thermalprinter.local'
@@ -9,26 +10,30 @@ def print_file(file_path, use_printer=False):
         print(f'Skipping printing file: {file_path}')
         return None
 
-    global _printer_connection
-    print_job_id = None
+    def _print():
+        global _printer_connection
+        print_job_id = None
 
-    if not _printer_connection:
-        try:
-            _printer_connection = cups.Connection(host=_cups_server)    
-        except Exception as e:
-            print(f'Error connecting to printer: {e}')
-            _printer_connection = None
+        if not _printer_connection:
+            try:
+                _printer_connection = cups.Connection(host=_cups_server)    
+            except Exception as e:
+                print(f'Error connecting to printer: {e}')
+                _printer_connection = None
+                return
 
-    if _printer_connection:
-        try:
-            print_options = {}
+        if _printer_connection:
+            try:
+                print_options = {}
 
-            # Options are now part of the default config on the CUPS server
-            # print_options = {'orientation-requested':'6'} #rotate 90 degrees counter clockwise, prints small
+                # Options are now part of the default config on the CUPS server
+                # print_options = {'orientation-requested':'6'} # rotate 90 degrees counterclockwise, prints small
 
-            print_job_id = _printer_connection.printFile(_printer_name, file_path, 'Print Job', print_options)
-        except Exception as e:
-            print(f'Error printing file: {e}')
-            _printer_connection = None
+                print_job_id = _printer_connection.printFile(_printer_name, file_path, 'Print Job', print_options)
+                print(f'Print job {print_job_id} submitted successfully')
+            except Exception as e:
+                print(f'Error printing file: {e}')
+                _printer_connection = None
 
-    return print_job_id
+    thread = threading.Thread(target=_print, daemon=True)
+    thread.start()
